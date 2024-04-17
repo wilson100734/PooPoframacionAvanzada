40a8981e692b18d574904a143a677c507e1c443b
wilson100734 <158371717+wilson100734@users.noreply.github.com>
2024-04-16 20:08:38 -0500

    Add files via upload

---
+++ crud-completion-main/crud-completion-main/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into "usuarios" ("name", "last_name", "age") values ('Wilson', 'Leguizamon', 30);
+insert into "usuarios" ("name", "last_name", "age") values ('Jorge', 'Izza', 50);
+insert into "usuarios" ("name", "last_name", "age") values ( 'Daniela', 'Asi', 79);
